4450664b179bd65db518062df99d0036c0ec5df7
Rodion <radzivon.sharapau@gmail.com>
2023-06-15 01:04:42 +0300

    fixed

--- Scavdue/Controllers/AdministrativeUnitController.cs
+++ Scavdue/Controllers/AdministrativeUnitController.cs
@@ -5,82 +5,83 @@ using Microsoft.AspNetCore.Mvc;
 using Scavdue.Business.Interfaces;
 using Scavdue.Consts;
 
-namespace Scavdue.Controllers
+namespace Scavdue.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AdministrativeUnitController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AdministrativeUnitController : ControllerBase
+    private readonly IAdministrativeUnitService _administrativeUnitService;
+    private readonly IUnitObjectsService _unitObjectsService;
+
+    public AdministrativeUnitController(IAdministrativeUnitService administrativeUnitService, IUnitObjectsService unitObjectsService)
     {
-        private readonly IAdministrativeUnitService _administrativeUnitService;
-        private readonly IUnitObjectsService _unitObjectsService;
-
-        public AdministrativeUnitController(IAdministrativeUnitService administrativeUnitService, IUnitObjectsService unitObjectsService)
-        {
-            _administrativeUnitService = administrativeUnitService;
-            _unitObjectsService = unitObjectsService;
-        }
-
-        [HttpGet("/AdministrativeUnits/{unitName}")]
-        public async Task<IActionResult> GetUnitsByName(string unitName)
-        {
-            var units = await _administrativeUnitService.GetUnitListByNameAsync(unitName);
-            return Ok(units);
-        }
-
-        [HttpGet("/AdministrativeUnits/Country/{countryId}/CityList")]
-        public async Task<IActionResult> GetCityList(int countryId)
-        {
-            var units = await _administrativeUnitService.GetCitiesList(countryId);
-            return Ok(units);
-        }
-
-        [HttpGet("/AdministrativeUnits/{unitId:int}/LifeIndex")]
-        public async Task<IActionResult> GetUnitLifeIndex(int unitId)
-        {
-            var unit = await _administrativeUnitService.GetUnitLifeIndex(unitId);
-            return Ok(unit);
-        }
-        
-        [HttpGet("/AdministrativeUnits/Country/{unitName}")]
-        public async Task<IActionResult> GetCountryUnit(string unitName)
-        {
-            var units = await _administrativeUnitService.GetCountryAsync(unitName);
-            return Ok(units);
-        }
-
-        [HttpGet("/AdministrativeUnits/{parentId:int}/ChildUnits")]
-        public async Task<IActionResult> GetChildUnits(int parentId)
-        {
-            var units = await _administrativeUnitService.GetChildsAsync(parentId);
-            return Ok(units);
-        }
-
-        [HttpGet("/AdministrativeUnits/{unitId:int}/UnitObjects")]
-        public async Task<IActionResult> GetUnitObjects(int unitId)
-        {
-            var units = await _unitObjectsService.GetUnitObjects(unitId);
-            return Ok(units);
-        }
-
-        [HttpGet("/AdministrativeUnits/Moderator/ComplexUpdate/{countryName}")]
-        public async Task<IActionResult> GetComplexUpdate(string countryName)
-        {
-            var message = await _administrativeUnitService.AdminComplexAdminUnits(countryName);
-            return Ok(message);
-        }
-
-        [HttpGet("/AdministrativeUnits/Moderator/UnitUpdate/{unitId:int}")]
-        public async Task<IActionResult> GetUnitUpdate(int unitId)
-        {
-            //var message = await _administrativeUnitService.AdminComplexAdminUnits(unitName);
-            return Ok();
-        }
-
-        /*[HttpGet("/AdministrativeUnits/Admin/TestUnits/")]
-        public async Task<IActionResult> GetComplexTestUnits()
-        {
-            var message = await _administrativeUnitService.TestUnits();
-            return Ok(message);
-        }*/
+        _administrativeUnitService = administrativeUnitService;
+        _unitObjectsService = unitObjectsService;
     }
+
+    [HttpGet("/AdministrativeUnits/{unitName}")]
+    public async Task<IActionResult> GetUnitsByName(string unitName)
+    {
+        var units = await _administrativeUnitService.GetUnitListByNameAsync(unitName);
+        return Ok(units);
+    }
+
+    [HttpGet("/AdministrativeUnits/Country/{countryId}/CityList")]
+    public async Task<IActionResult> GetCityList(int countryId)
+    {
+        var units = await _administrativeUnitService.GetCitiesList(countryId);
+        return Ok(units);
+    }
+
+    [HttpGet("/AdministrativeUnits/{unitId:int}/LifeIndex")]
+    public async Task<IActionResult> GetUnitLifeIndex(int unitId)
+    {
+        var unit = await _administrativeUnitService.GetUnitLifeIndex(unitId);
+        return Ok(unit);
+    }
+    
+    [HttpGet("/AdministrativeUnits/Country/{unitName}")]
+    public async Task<IActionResult> GetCountryUnit(string unitName)
+    {
+        var units = await _administrativeUnitService.GetCountryAsync(unitName);
+        return Ok(units);
+    }
+
+    [HttpGet("/AdministrativeUnits/{parentId:int}/ChildUnits")]
+    public async Task<IActionResult> GetChildUnits(int parentId)
+    {
+        var units = await _administrativeUnitService.GetChildsAsync(parentId);
+        return Ok(units);
+    }
+
+    [HttpGet("/AdministrativeUnits/{unitId:int}/UnitObjects")]
+    public async Task<IActionResult> GetUnitObjects(int unitId)
+    {
+        var units = await _unitObjectsService.GetUnitObjects(unitId);
+        return Ok(units);
+    }
+
+    [Authorize(Policy = APIPolicies.AdminPolicy)]
+    [HttpGet("/AdministrativeUnits/Admin/ComplexUpdate/{countryName}")]
+    public async Task<IActionResult> GetComplexUpdate(string countryName)
+    {
+        var message = await _administrativeUnitService.AdminComplexAdminUnits(countryName);
+        return Ok(message);
+    }
+
+    [Authorize(Policy = APIPolicies.ModeratorPolicy)]
+    [HttpGet("/AdministrativeUnits/Moderator/UnitUpdate/{unitId:int}")]
+    public async Task<IActionResult> GetUnitUpdate(int unitId)
+    {
+        //var message = await _administrativeUnitService.AdminComplexAdminUnits(unitName);
+        return Ok();
+    }
+
+    /*[HttpGet("/AdministrativeUnits/Admin/TestUnits/")]
+    public async Task<IActionResult> GetComplexTestUnits()
+    {
+        var message = await _administrativeUnitService.TestUnits();
+        return Ok(message);
+    }*/
 }
--- Scavdue/Controllers/UnitObjectController.cs
+++ Scavdue/Controllers/UnitObjectController.cs
@@ -3,24 +3,23 @@ using Microsoft.AspNetCore.Mvc;
 using Scavdue.Business.Interfaces;
 using Scavdue.Business.Services;
 
-namespace Scavdue.Controllers
+namespace Scavdue.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UnitObjectController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UnitObjectController : ControllerBase
+    private readonly IUnitObjectsService _unitObjectsService;
+
+    public UnitObjectController(IUnitObjectsService unitObjectsService)
     {
-        private readonly IUnitObjectsService _unitObjectsService;
+        _unitObjectsService = unitObjectsService;
+    }
 
-        public UnitObjectController(IUnitObjectsService unitObjectsService)
-        {
-            _unitObjectsService = unitObjectsService;
-        }
-
-        [HttpGet("/AdministrativeUnits/{unitId:int}/ObjectsCount")]
-        public async Task<IActionResult> GetChildUnits(int unitId)
-        {
-            var units = await _unitObjectsService.GetBuildingClassCount(unitId);
-            return Ok(units);
-        }
+    [HttpGet("/AdministrativeUnits/{unitId:int}/ObjectsCount")]
+    public async Task<IActionResult> GetChildUnits(int unitId)
+    {
+        var units = await _unitObjectsService.GetBuildingClassCount(unitId);
+        return Ok(units);
     }
 }
